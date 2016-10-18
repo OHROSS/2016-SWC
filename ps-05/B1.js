6f23d53f09dcc4826ecf8011d33a7ec5d13f4448
Vasyl Kochura <vasylkochura@gmail.com>
2016-10-18 19:01:28 -0400

    b1

--- ps-05/B1.js
+++ ps-05/B1.js
@@ -1,12 +1,19 @@
 var canvas = document.getElementById('canvas');
 var drawingPad = canvas.getContext('2d');
 
-drawingPad.beginPath();
-drawingPad.moveTo(0,0);
-for( var y=0; y < 500; y++){
+
+for( var y=0; y < 333; y++){
   drawingPad.beginPath();
-  drawingPad.moveTo(0,0);
+  drawingPad.moveTo(167,0);
   drawingPad.lineTo(500,y);
+  drawingPad.strokeStyle="black";
+  drawingPad.stroke();
+}
+
+for( var x=0; x < 333; x++){
+  drawingPad.beginPath();
+  drawingPad.moveTo(0,167);
+  drawingPad.lineTo(x,500);
   drawingPad.strokeStyle="black";
   drawingPad.stroke();
 }
